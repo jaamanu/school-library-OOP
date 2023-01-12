d5c881a1e1fdeaac34b4d0507304625d59577161
jaamanu <jaamanu@gmail.com>
2023-01-12 13:52:39 +0200

    testing

--- models/classroom.rb
+++ models/classroom.rb
@@ -1,18 +1,14 @@
-require_relative 'student'
-
 class Classroom
-  # setters & getters
-  attr_accessor :label, :students
+  attr_accessor :label
+  attr_reader :students
 
-  # constructor
   def initialize(label)
     @label = label
     @students = []
   end
 
-  # add student method
   def add_student(student)
-    @students.push(student) unless @students.include?(student)
+    @students.push(student)
     student.classroom = self
   end
 end
--- models/rental.rb
+++ models/rental.rb
@@ -1,17 +1,9 @@
 class Rental
-  # setters & getters
   attr_accessor :date, :book, :person
 
-  # constructor
   def initialize(date, book, person)
     @date = date
     @book = book
-    book.rentals << self
     @person = person
-    person.rentals << self
-  end
-
-  def to_s
-    "Date: #{@date}, Book: \"#{book.title}\" by #{@book.author}"
   end
 end
